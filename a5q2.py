412bc9a98e71dfcb55f52f45f86156732c10a3e4
Harry6542 <ozc189@usak.ca>
2023-08-07 14:03:59 -0600

    defined diff_sum_postorder() and written code for it

--- a5q2.py
+++ a5q2.py
@@ -3,6 +3,8 @@ from copy import copy
 
 
 class Treenode(object):
+
+    # ... (class definition remains the same)
     def subst(self, t, r):
         """
         Substitute a target value t with a replacement value r wherever it appears in the given tree.
@@ -16,10 +18,10 @@ class Treenode(object):
         self.subst(self.get_right(), t, r)
 
     def copy(tnode):
-            """
-            Create an exact copy of the given tree with new treenodes and the same data values.
-            Returns a reference to the new tree.
-            """
+        """
+        Create an exact copy of the given tree with new treenodes and the same data values.
+        Returns a reference to the new tree.
+        """
         if tnode is None:
             return None
         new_node = Treenode(tnode.get_data())
@@ -27,32 +29,41 @@ class Treenode(object):
         new_node.set_right(copy(tnode.get_right()))
         return new_node
 
+    def diff_sum_preorder(tnode):
+        """
+        Traverse the tree in preorder and alternate between finding the difference and summation of values.
+        Returns the value pattern X - Y + Z.
+        """
+        if tnode is None:
+            return 0
+        x = tnode.get_data()
+        y = tnode.diff_sum_preorder(tnode.get_left())
+        z = tnode.diff_sum_preorder(tnode.get_right())
+        return x - y + z
 
-   def diff_sum_preorder(tnode):
-       """
-          Traverse the tree in preorder and alternate between finding the difference and summation of values.
-          Returns the value pattern X - Y + Z.
-          """
+    def diff_sum_inorder(tnode):
+        """
+        Traverse the tree in inorder and alternate between finding the difference and summation of values.
+        Returns the value pattern X - Y + Z.
+        """
+        if tnode is None:
+            return 0
+        x = tnode.diff_sum_inorder(tnode.get_left())
+        y = tnode.get_data()
+        z = tnode.diff_sum_inorder(tnode.get_right())
+        return x - y + z
 
-       if tnode is None:
-           return 0
-       x = tnode.get_data()
-       y = tnode.diff_sum_preorder(tnode.get_left())
-       z = tnode.diff_sum_preorder(tnode.get_right())
-       return x - y + z
-
-
-   def diff_sum_inorder(tnode):
-       """
-    Traverse the tree in inorder and alternate between finding the difference and summation of values.
-    Returns the value pattern X - Y + Z.
-       """
-       if tnode is None:
-           return 0
-       x = tnode.diff_sum_inorder(tnode.get_left())
-       y = tnode.get_data()
-       z = tnode.diff_sum_inorder(tnode.get_right())
-       return x - y + z
+    def diff_sum_postorder(tnode):
+        """
+        Traverse the tree in postorder and alternate between finding the difference and summation of values.
+        Returns the value pattern X - Y + Z.
+        """
+        if tnode is None:
+            return 0
+        x = tnode.diff_sum_postorder(tnode.get_left())
+        y = tnode.diff_sum_postorder(tnode.get_right())
+        z = tnode.get_data()
+        return x - y + z
 
 
 
