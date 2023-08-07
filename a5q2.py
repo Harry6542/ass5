7e56d66911629fa08ddcb0481522ee4660ebd853
Harry6542 <ozc189@usak.ca>
2023-08-07 13:49:32 -0600

    defined subst and written code for it

--- a5q2.py
+++ a5q2.py
@@ -3,3 +3,14 @@ from copy import copy
 
 
 class Treenode(object):
+    def subst(self, t, r):
+        """
+        Substitute a target value t with a replacement value r wherever it appears in the given tree.
+        Modifies the given tree in place.
+        """
+        if self is None:
+            return
+        if self.get_data() == t:
+            self.set_data(r)
+        self.subst(self.get_left(), t, r)
+        self.subst(self.get_right(), t, r)
