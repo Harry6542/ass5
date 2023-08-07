8c2159ed9ac153bd122dd00b0d7fe19af41fe466
Harry6542 <ozc189@usak.ca>
2023-08-07 14:13:40 -0600

    def is_ordered(): and written code for it

--- a5q3.py
+++ a5q3.py
@@ -9,3 +9,15 @@ class TreeNode:
         self.left = None
         self.right = None
 def ordered(tnode):
+    def is_ordered(node, lower_bound, upper_bound):
+        if node is None:
+            return True
+
+        if lower_bound is not None and node.data <= lower_bound:
+            return False
+
+        if upper_bound is not None and node.data >= upper_bound:
+            return False
+
+        left_ordered = is_ordered(node.left, lower_bound, node.data)
+        right_ordered = is_ordered(node.right, node.data, upper_bound)
