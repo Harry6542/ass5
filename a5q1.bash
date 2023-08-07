8c6aafa72174031c98dff563a5e2c90bb9002e1b
Harry6542 <ozc189@usak.ca>
2023-08-07 10:39:01 -0600

    q4 completed

--- a5q1.bash
+++ a5q1.bash
@@ -33,4 +33,15 @@ ozc189@tux8:~/ozc189_green> ps
  601816 pts/85   00:00:00 bash
  688907 pts/85   00:00:00 ps
 [1]+  Done                    sleep 5
+ozc189@tux8:~/ozc189_green> top
+top - 10:38:30 up 100 days, 17:40, 12 users,  load average: 0.28, 0.38, 0.35
+Tasks: 1691 total,   1 running, 1598 sleeping,   5 stopped,  87 zombie
+%Cpu(s):  0.2 us,  0.3 sy,  0.0 ni, 99.5 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+MiB Mem : 386833.9 total, 331269.8 free,  10940.8 used,  44623.3 buff/cache
+MiB Swap: 152576.0 total, 152501.2 free,     74.8 used. 372213.5 avail Mem
+
+    PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND
+    232 root       0 -20       0      0      0 I   0.0   0.0   0:00.00 kworker/42:0H-events_highpri
+    233 root      20   0       0      0      0 S   0.0   0.0   0:00.00 cpuhp/43
+    234 root      rt   0       0      0      0 S   0.0   0.0   0:11.09 migration/43
 
