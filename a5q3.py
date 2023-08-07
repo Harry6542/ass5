383e491d3bab010d58705355892a2b8ba7619dc3
Harry6542 <ozc189@usak.ca>
2023-08-07 14:08:45 -0600

    Class TreeNode

---
+++ a5q3.py
@@ -0,0 +1,10 @@
+#Name -Harry Patel
+#NSID-ozc189
+#student Number-11358887
+#course- Cmpt 145
+
+class TreeNode:
+    def _init_(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
