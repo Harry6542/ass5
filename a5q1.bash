bc1d31f027c5d302d462ecd3a16884c6d719d2a0
Harry6542 <ozc189@usak.ca>
2023-08-07 10:29:24 -0600

    ls -a command

---
+++ a5q1.bash
@@ -0,0 +1,14 @@
+(ass5) PS C:\Users\varsha\PycharmProjects\ass5> ssh ozc189@tuxworld.usask.ca
+ozc189@tuxworld.usask.ca's password:
+Last login: Mon Aug  7 09:14:26 2023 from 174.2.22.242
+ozc189@tux8:~> pwd
+/student/ozc189
+ozc189@tux8:~> script a5q1_log.txt
+Script started, output log file is 'a5q1_log.txt'.
+ozc189@tux8:~> ls -a
+ ./             .config/     Downloads/   .login*    'Saved Games'/                                                 Videos/
+ ../            Contacts/    Favorites/   Music/      Searches/
+ a5q1_log.txt   .cshrc*      .java/       Pictures/   Templates/
+ .bashrc*       Desktop/     Links/       .profile@   .uc-72e3792250a3e57aa35f03e76d96b69f.ozc189.s360-peon12.tmp
+ .cache/        Documents/   .local/      Public/     .uc-72e3792250a3e57aa35f03e76d96b69f.ozc189.s360-peon22.tmp
+
